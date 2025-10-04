4848df3da1c7e42ea88207471e62bba3318a95b7
Sued Cruz <dksrasu@gmail.com>
2025-10-04 23:11:28 +0000

    concerte

--- src/components/layout/snowfall.tsx
+++ src/components/layout/snowfall.tsx
@@ -4,27 +4,38 @@ import { useEffect, useState } from 'react';
 
 const Snowfall = () => {
   const [snowflakes, setSnowflakes] = useState<JSX.Element[]>([]);
+  const [isClient, setIsClient] = useState(false);
 
   useEffect(() => {
-    const createSnowflakes = () => {
-      const flakeCount = 50;
-      const newSnowflakes = Array.from({ length: flakeCount }).map((_, i) => {
-        const size = Math.random() * 5 + 2; // size between 2px and 7px
-        const style = {
-          width: `${size}px`,
-          height: `${size}px`,
-          left: `${Math.random() * 100}%`,
-          animationDuration: `${Math.random() * 10 + 5}s`, // duration between 5s and 15s
-          animationDelay: `${Math.random() * 5}s`,
-          opacity: Math.random() * 0.5 + 0.3, // opacity between 0.3 and 0.8
-        };
-        return <div key={i} className="snowflake" style={style}></div>;
-      });
-      setSnowflakes(newSnowflakes);
-    };
-
-    createSnowflakes();
+    setIsClient(true);
   }, []);
+
+  useEffect(() => {
+    if (isClient) {
+      const createSnowflakes = () => {
+        const flakeCount = 50;
+        const newSnowflakes = Array.from({ length: flakeCount }).map((_, i) => {
+          const size = Math.random() * 5 + 2; // size between 2px and 7px
+          const style = {
+            width: `${size}px`,
+            height: `${size}px`,
+            left: `${Math.random() * 100}%`,
+            animationDuration: `${Math.random() * 10 + 5}s`, // duration between 5s and 15s
+            animationDelay: `${Math.random() * 5}s`,
+            opacity: Math.random() * 0.5 + 0.3, // opacity between 0.3 and 0.8
+          };
+          return <div key={i} className="snowflake" style={style}></div>;
+        });
+        setSnowflakes(newSnowflakes);
+      };
+
+      createSnowflakes();
+    }
+  }, [isClient]);
+
+  if (!isClient) {
+    return null;
+  }
 
   return <div className="snow no-print">{snowflakes}</div>;
 };
