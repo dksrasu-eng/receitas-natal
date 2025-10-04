41ec14a64fdb09df357279373b9211c0cfff011d
Sued Cruz <dksrasu@gmail.com>
2025-10-04 23:07:40 +0000

    a imagem não está aparecendo

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 's2-casavogue.glbimg.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
