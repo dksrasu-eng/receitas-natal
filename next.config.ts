6a0f7267cf4a22813bf4d424422ec5f3f91745d3
Sued Cruz <dksrasu@gmail.com>
2025-10-05 06:40:09 +0000

    resolva esse problema, dps teste todo o site e s etiver outro problema r

--- next.config.ts
+++ next.config.ts
@@ -16,26 +16,17 @@ const nextConfig: NextConfig = {
       { protocol: 'https', hostname: 'picsum.photos' },
       
       // Stock photos & CDNs
-      { protocol: 'https', hostname: 'img.freepik.com' },
       { protocol: 'https', hostname: 'images.unsplash.com' },
       { protocol: 'https', hostname: 'images.ctfassets.net' },
-      { protocol: 'https', hostname: 'p2.trrsf.com' },
-      { protocol: 'https', hostname: 'i0.wp.com' },
       { protocol: 'https', hostname: 'encrypted-tbn0.gstatic.com' },
-      { protocol: 'https', hostname: 'sabores-new.s3.amazonaws.com' },
-      { protocol: 'https', hostname: 'lirp.cdn-website.com' },
-      { protocol: 'https', hostname: 'i.panelinha.com.br' },
-      { protocol: 'https', hostname: 'docepedia.com' },
-      { protocol: 'https', hostname: 'bolinhodecoco.pt' },
-      { protocol: 'https', hostname: 'smartfarmer.pt' },
-      { protocol: 'https', hostname: 'www.nattyspantry.com' },
-      { protocol: 'https', hostname: 'amodadoflavio.pt' },
+      { protocol: 'https', hostname: 'i.s3.glbimg.com' },
 
-      // Wildcards for Major Brazilian Recipe/Media Sites
+      // Wildcards for Major Brazilian/International Recipe/Media Sites
       { protocol: 'https', hostname: '**.com.br' },
       { protocol: 'https', hostname: '**.glbimg.com' },
-      { protocol: 'https', hostname: '**.tudoreceitas.com' },
       { protocol: 'https', hostname: '**.static.cordonbleu.edu' },
+      { protocol: 'https', hostname: '**.com' },
+      { protocol: 'https', hostname: '**.pt' },
     ],
   },
 };
