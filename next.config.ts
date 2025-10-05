fde60aef5b21d23bd1d2ddf62eab8bf7b04def27
Sued Cruz <dksrasu@gmail.com>
2025-10-05 04:44:27 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -11,254 +11,67 @@ const nextConfig: NextConfig = {
   },
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-      },
-      {
-        protocol: 'https',
-        hostname: 's2-casavogue.glbimg.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.s3.glbimg.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'minhasreceitinhas.com.br',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.sadia.com.br',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn0.tudoreceitas.com',
-      },
-       {
-        protocol: 'https',
-        hostname: 'melevacontigo.com.br',
-      },
-       {
-        protocol: 'https',
-        hostname: 'sabores-new.s3.amazonaws.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'static.itdg.com.br',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn0.umcomo.com.br',
-      },
-      {
-        protocol: 'https',
-        hostname: 'encrypted-tbn0.gstatic.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'chaparadois.com.br',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.vilma.com.br',
-      },
-      {
-        protocol: 'https',
-        hostname: 'naturaves.com.br',
-      },
-      {
-        protocol: 'https',
-        hostname: 'sabordasespeciarias.com.br',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.oitedi.com.br',
-      },
-      {
-        protocol: 'https',
-        hostname: 'conteudo.imguol.com.br',
-      },
-      {
-        protocol: 'https',
-        hostname: 'boomi.b-cdn.net',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.comidaereceitas.com.br',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.academiaassai.com.br',
-      },
-      {
-        protocol: 'https',
-        hostname: 'receitasobremesa.com.br',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i0.wp.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.wine.com.br',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.dicasdetreino.com.br',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.receitadevovo.com.br',
-      },
-      {
-        protocol: 'https',
-        hostname: 'd2qcpt1idvpipw.cloudfront.net',
-      },
-      {
-        protocol: 'https',
-        hostname: 'anamariareceitas.com.br',
-      },
-      {
-        protocol: 'https',
-        hostname: 'i.panelinha.com.br',
-      },
-      {
-        protocol: 'https',
-        hostname: 'benditosalgado.com.br',
-      },
-      {
-        protocol: 'https',
-        hostname: 'static.cordonbleu.edu',
-      },
-      {
-        protocol: 'https',
-        hostname: 'img.freepik.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cozinhafitsemsegredos.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.entrepratosecopos.com.br',
-      },
-      {
-        protocol: 'https',
-        hostname: 'assets.unileversolutions.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'bolinhodecoco.pt',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.receitasnestle.com.br',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.seara.com.br',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.rampinelli.com.br',
-      },
-      {
-        protocol: 'https',
-        hostname: 'amopaocaseiro.com.br',
-      },
-      {
-        protocol: 'https',
-        hostname: 'lirp.cdn-website.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'pt.petitchef.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.doceiguaria.com.br',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.manusmenu.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'delicious.com.br',
-      },
-      {
-        protocol: 'https',
-        hostname: 'docepedia.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'smartfarmer.pt',
-      },
-      {
-        protocol: 'https',
-        hostname: 'p2.trrsf.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'redacaors.com.br',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.mendezalimentos.com.br',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.nattyspantry.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'amodadoflavio.pt',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.tribunapr.com.br',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.ctfassets.net',
-      },
-      {
-        protocol: 'https',
-        hostname: 'lechef.com.br',
-      },
-      {
-        protocol: 'https',
-        hostname: 'www.estadao.com.br',
-      },
-      {
-        protocol: 'https',
-        hostname: 'recipesblob.oetker.com.br',
-      },
-      {
-        protocol: 'https',
-        hostname: 'guiadacozinha.com.br',
-      },
-      {
-        protocol: 'https',
-        hostname: 'engenhocaracuipe.com.br',
-      },
-      {
-        protocol: 'https',
-        hostname: 'baggiocafe.com.br',
-      },
-      {
-        protocol: 'https',
-        hostname: 'doiscunhados.com.br',
-      },
+      { protocol: 'https', hostname: 'placehold.co' },
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'picsum.photos' },
+      { protocol: 'https', hostname: '**.glbimg.com' },
+      { protocol: 'https', hostname: 'minhasreceitinhas.com.br' },
+      { protocol: 'https', hostname: '**.sadia.com.br' },
+      { protocol: 'https', hostname: '**.tudoreceitas.com' },
+      { protocol: 'https', hostname: 'melevacontigo.com.br' },
+      { protocol: 'https', hostname: 'sabores-new.s3.amazonaws.com' },
+      { protocol: 'https', hostname: '**.itdg.com.br' },
+      { protocol: 'https', hostname: '**.umcomo.com.br' },
+      { protocol: 'https', hostname: 'encrypted-tbn0.gstatic.com' },
+      { protocol: 'https', hostname: 'chaparadois.com.br' },
+      { protocol: 'https', hostname: '**.vilma.com.br' },
+      { protocol: 'https', hostname: 'naturaves.com.br' },
+      { protocol: 'https', hostname: 'sabordasespeciarias.com.br' },
+      { protocol: 'https', hostname: '**.oitedi.com.br' },
+      { protocol: 'https', hostname: 'conteudo.imguol.com.br' },
+      { protocol: 'https', hostname: 'boomi.b-cdn.net' },
+      { protocol: 'https', hostname: '**.comidaereceitas.com.br' },
+      { protocol: 'https', hostname: '**.academiaassai.com.br' },
+      { protocol: 'https', hostname: 'receitasobremesa.com.br' },
+      { protocol: 'https', hostname: 'i0.wp.com' },
+      { protocol: 'https', hostname: '**.wine.com.br' },
+      { protocol: 'https', hostname: '**.dicasdetreino.com.br' },
+      { protocol: 'https', hostname: '**.receitadevovo.com.br' },
+      { protocol: 'https', hostname: 'd2qcpt1idvpipw.cloudfront.net' },
+      { protocol_host: 'https', hostname: 'anamariareceitas.com.br' },
+      { protocol: 'https', hostname: '**.panelinha.com.br' },
+      { protocol: 'https', hostname: 'benditosalgado.com.br' },
+      { protocol: 'https', hostname: '**.cordonbleu.edu' },
+      { protocol: 'https', hostname: '**.freepik.com' },
+      { protocol: 'https', hostname: 'cozinhafitsemsegredos.com' },
+      { protocol: 'https', hostname: '**.entrepratosecopos.com.br' },
+      { protocol: 'https', hostname: 'assets.unileversolutions.com' },
+      { protocol: 'https', hostname: 'bolinhodecoco.pt' },
+      { protocol: 'https', hostname: '**.receitasnestle.com.br' },
+      { protocol: 'https', hostname: '**.seara.com.br' },
+      { protocol: 'https', hostname: '**.rampinelli.com.br' },
+      { protocol: 'https', hostname: 'amopaocaseiro.com.br' },
+      { protocol: 'https', hostname: 'lirp.cdn-website.com' },
+      { protocol: 'https', hostname: 'pt.petitchef.com' },
+      { protocol: 'https', hostname: '**.doceiguaria.com.br' },
+      { protocol: 'https', hostname: '**.manusmenu.com' },
+      { protocol: 'https', hostname: 'delicious.com.br' },
+      { protocol: 'https', hostname: 'docepedia.com' },
+      { protocol: 'https', hostname: 'smartfarmer.pt' },
+      { protocol: 'https', hostname: 'p2.trrsf.com' },
+      { protocol: 'https', hostname: 'redacaors.com.br' },
+      { protocol: 'https', hostname: '**.mendezalimentos.com.br' },
+      { protocol: 'https', hostname: '**.nattyspantry.com' },
+      { protocol: 'https', hostname: 'amodadoflavio.pt' },
+      { protocol: 'https', hostname: '**.tribunapr.com.br' },
+      { protocol: 'https', hostname: 'images.ctfassets.net' },
+      { protocol: 'https', hostname: 'lechef.com.br' },
+      { protocol: 'https', hostname: '**.estadao.com.br' },
+      { protocol: 'https', hostname: 'recipesblob.oetker.com.br' },
+      { protocol: 'https', hostname: 'guiadacozinha.com.br' },
+      { protocol: 'https', hostname: 'engenhocaracuipe.com.br' },
+      { protocol: 'https', hostname: 'baggiocafe.com.br' },
+      { protocol: 'https', hostname: 'doiscunhados.com.br' },
     ],
   },
 };
