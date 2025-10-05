713794370a1462501c96b7b150af6c2f67b9b023
Sued Cruz <dksrasu@gmail.com>
2025-10-05 06:07:30 +0000

    concerte e deixe o site totalmente funcional novamente

--- next.config.ts
+++ next.config.ts
@@ -34,7 +34,8 @@ const nextConfig: NextConfig = {
 
       // Other specific domains
       { protocol: 'https', hostname: 'docepedia.com' },
-      { protocol: 'https', hostname: 'cozinhafitsemsegredos.com' }
+      { protocol: 'https', hostname: 'cozinhafitsemsegredos.com' },
+      { protocol: 'https', hostname: 'minhasreceitinhas.com.br' }
     ],
   },
 };
