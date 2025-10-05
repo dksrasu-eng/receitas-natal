b82b076742c9d600e33ddac347ecd1fa05ffc28c
Sued Cruz <dksrasu@gmail.com>
2025-10-05 05:45:31 +0000

    import type { NextConfig } from 'next';
    const nextConfig: NextConfig =

---
+++ src/next.config.ts
@@ -0,0 +1,65 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'placehold.co' },
+      { protocol: 'https', hostname: 'picsum.photos' },
+      { protocol: 'https', hostname: 'img.freepik.com' },
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'images.ctfassets.net' },
+      { protocol: 'https', hostname: 'p2.trrsf.com' },
+      { protocol: 'https', hostname: 'amopaocaseiro.com.br' },
+      { protocol: 'https', hostname: 'pt.petitchef.com' },
+      { protocol: 'https', hostname: 'docepedia.com' },
+      { protocol: 'https', hostname: 'smartfarmer.pt' },
+      { protocol: 'https', hostname: 'redacaors.com.br' },
+      { protocol: 'https', hostname: 'lechef.com.br' },
+      { protocol: 'https', hostname: 'chaparadois.com.br' },
+      { protocol: 'https', hostname: 'melevacontigo.com.br' },
+      { protocol: 'https', hostname: 'delicious.com.br' },
+      { protocol: 'https', hostname: 'bolinhodecoco.pt' },
+      { protocol: 'https', hostname: 'i0.wp.com' },
+      { protocol: 'https', hostname: '**.glbimg.com' },
+      { protocol: 'https', hostname: '**.sadia.com.br' },
+      { protocol: 'https', hostname: '**.tudoreceitas.com' },
+      { protocol: 'https', hostname: '**.itdg.com.br' },
+      { protocol: 'https', hostname: '**.umcomo.com.br' },
+      { protocol: 'https', hostname: '**.vilma.com.br' },
+      { protocol: 'https', hostname: '**.oitedi.com.br' },
+      { protocol: 'https', hostname: '**.comidaereceitas.com.br' },
+      { protocol: 'https', hostname: '**.seara.com.br' },
+      { protocol: 'https', hostname: '**.rampinelli.com.br' },
+      { protocol: 'https', hostname: '**.receitasnestle.com.br' },
+      { protocol: 'https', hostname: 'conteudo.imguol.com.br' },
+      { protocol: 'https', hostname: 'www.receitadevovo.com.br' },
+      { protocol: 'https', hostname: 'anamariareceitas.com.br' },
+      { protocol: 'https', hostname: 'www.academiaassai.com.br' },
+      { protocol: 'https', hostname: 'receitasobremesa.com.br' },
+      { protocol: 'https', hostname: 'www.wine.com.br' },
+      { protocol: 'https', hostname: 'i.panelinha.com.br' },
+      { protocol: 'https', hostname: 'static.cordonbleu.edu' },
+      { protocol: 'https', hostname: 'www.entrepratosecopos.com.br' },
+      { protocol: 'https', hostname: 'www.mendezalimentos.com.br' },
+      { protocol: 'https', hostname: 'amodadoflavio.pt' },
+      { protocol: 'https', hostname: 'www.tribunapr.com.br' },
+      { protocol: 'https', hostname: 'www.estadao.com.br' },
+      { protocol: 'https', hostname: 'lirp.cdn-website.com' },
+      { protocol: 'https', hostname: 'sabores-new.s3.amazonaws.com' },
+      { protocol: 'https', hostname: 'naturaves.com.br' },
+      { protocol: 'https', hostname: 'sabordasespeciarias.com.br' },
+      { protocol: 'https', hostname: 'encrypted-tbn0.gstatic.com' },
+      { protocol: 'https', hostname: 'www.nattyspantry.com'},
+    ],
+  },
+};
+
+export default nextConfig;
